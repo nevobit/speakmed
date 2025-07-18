000fa520cf9c236858ebb239210478debc7cba71
Bitter <yunsde18@gmail.com>
2025-07-18 07:43:22 -0500

    chore: info

--- apps/portal/src/screens/ErrorBoundary/index.tsx
+++ apps/portal/src/screens/ErrorBoundary/index.tsx
@@ -1,15 +1,14 @@
-import { Button } from '@repo/design-system/web';
 import styles from './ErrorBoundary.module.css';
 
 const ErrorBoundary = () => {
   return (
     <div className={styles.container} role="alert">
-      <h3>Oops, that´s our bad </h3>
-      <p>Were not exactly sure what happend, but something went wrong.</p>
-      <p>
-        Uf you need innmediate helm please <a href="/">let us know</a>
-      </p>
-      <Button>Try again</Button>
+      <h3>Bienvenido </h3>
+      {/* <p>Were not exactly sure what happend, but something went wrong.</p> */}
+      {/* <p> */}
+        {/* Uf you need innmediate helm please <a href="/">let us know</a> */}
+      {/* </p> */}
+      <a href='/' >Ir al Dashboard</a>
     </div>
   );
 };
